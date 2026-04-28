31f20a2f70dfcf519969b39354199f4856c5d1b7
SAMAKI NYA ZENOUBA RHYADE_24G2773 <samakirhyade@gmail.com>
2026-04-28 17:14:34 +0100

    Premier commit de RSafeTrack

---
+++ RSafeTrack/RSafeTrack.py
@@ -0,0 +1,85 @@
+from flask import Flask, render_template, request, redirect, url_for
+import csv
+import os
+from datetime import datetime
+
+RSafeTrack = Flask(__name__)
+FICHIER_DONNEES = "RSafeTrack_global.csv"
+
+# Configuration par défaut (sera modifiée par l’utilisateur sur le site)
+Config_app = {
+    "periode": "Mensuel",
+    "budget": 5000.0
+}
+
+def charger_donnees():
+    Lignes = []
+    Total_general = 0
+    if os.path.exists(FICHIER_DONNEES):
+        with open(FICHIER_DONNEES, mode='r', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                try:
+                    # Structure CSV : Nom, Date, Catégorie, Montant, Statut
+                    M = float(row[3])
+                    Total_general += M
+                    Lignes.append({
+                        "nom": row[0],
+                        "date": row[1],
+                        "categorie": row[2],
+                      
+                                "montant": M,
+                        "statut": row[4]
+                    })
+                except:
+
+                 continue
+    return Lignes, Total_general
+
+@RSafeTrack.route('/')
+def index():
+    Historique, Total = charger_donnees()
+    Budget = Config_app["budget"]
+    Reste = Budget - Total
+    
+    # Message global pour le propriétaire (Toi)
+    if Total > Budget:
+        Msg = f"Alerte: Budget global dépassé de {abs(Reste)} XAF"
+        Style = "danger"
+    else:
+        Msg = f"Économie globale: {Reste} XAF"
+        Style = "success"
+
+    return render_template('RSafeTrack.html',Depenses=Historique, Total=Total, budget=Budget, Periode=Config_app["periode"], message=Msg, style=Style)
+
+@RSafeTrack.route('/ajouter', methods=['POST'])
+def ajouter():
+    Nom = request.form.get('nom_utilisateur')
+    Cat = request.form.get('categorie')
+    Mont = float(request.form.get('montant', 0))
+    date_j = datetime.now().strftime("%d/%m/%Y %H:%M")
+    
+    # Calcul du statut pour cette dépense précise
+    Budget = Config_app["budget"]
+    if Mont > (Budget* 0.5): # Si une seule dépense fait plus de 50% du budget
+        Statut = "DÉPENSE LOURDE"
+    elif Mont > 0:
+        Statut = "GESTION OK"
+    else:
+        Statut = "INVALIDE"
+
+    # Enregistrement dans la base de données csv
+    with open(FICHIER_DONNEES, mode='a', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow([Nom, date_j, Cat, Mont, Statut])
+        
+    return redirect(url_for('index'))
+
+@RSafeTrack.route('/configurer', methods=['POST'])
+def configurer():
+    Config_app["periode"] = request.form.get('periode')
+    Config_app["budget"] = float(request.form.get('budget', 5000))
+    return redirect(url_for('index'))
+
+if __name__ == "__main__":
+    RSafeTrack.run(debug=True)
---
+++ RSafeTrack/app.py
@@ -0,0 +1,105 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+
+App = Flask(__name__)
+
+# Connexion base de données
+def get_db():
+    Conn = sqlite3.connect("database.db")
+    Conn.row_factory = sqlite3.Row
+    return Conn
+
+# Création table
+def init_db():
+    Conn = get_db()
+    Conn.execute("""
+        CREATE TABLE IF NOT EXISTS depenses (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Montant REAL,
+            Categorie TEXT,
+            Date TEXT,
+            Description TEXT
+        )
+    """)
+    Conn.execute ("""
+                  CREATE TABLE IF NOT EXISTS configuration
+                  (solde_initial REAL)
+                  """)
+    Conn.commit
+    Conn.close()
+    
+
+init_db()
+# Page principale (formulaire)
+@App.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        Montant = request.form["montant"]
+        Categorie = request.form["categorie"]
+        Date = request.form["date"]
+        Description = request.form["description"]
+
+        if not Montant:
+            return "Erreur: montant obligatoire"
+
+        Conn = get_db()
+        Conn.execute(
+            "insert INTO depenses (montant, categorie, date, description) VALUES (?,?,?,?)",
+            (Montant, Categorie, Date, Description)
+        )
+        Conn.commit()
+        Conn.close()
+
+        return redirect("/dashboard")
+
+    return render_template("RSafeTrack.html")
+@App.route('/set_budget', methods=['POST'])
+def set_budget():
+    Budget = request.form.get('budget')
+    Conn = get_db()
+    # On efface l’ancien budget pour ne garder que le nouveau
+    Conn.execute("DELETE FROM configuration") 
+    Conn.execute("INSERT INTO configuration (solde_initial) VALUES (?)", (Budget,))
+    Conn.commit()
+    Conn.close()
+    return redirect('/dashboard')
+
+
+# Dashboard (analyse)
+@App.route("/dashboard")
+def dashboard():
+    Conn = get_db()
+
+    res_Total= Conn.execute("SELECT SUM(montant) FROM depenses").fetchone()
+    Total=res_Total[0] if res_Total[0] else 0
+
+    res_Moyenne = Conn.execute("SELECT AVG(montant) FROM depenses").fetchone()
+    Moyenne=res_Moyenne[0] if res_Moyenne and res_Moyenne[0] is not None else 0
+    
+    cat_rows= Conn.execute("""
+        SELECT categorie, SUM(montant) as Total
+        FROM depenses
+        GROUP BY categorie
+    """).fetchall()
+    Categories=[[r[0],r[1]] for r in cat_rows]
+
+
+    date_rows = Conn.execute("""
+        SELECT date, SUM(montant) as Total
+        FROM depenses
+        GROUP BY date
+    """).fetchall()
+    Dates=[[r[0],r[1]] for r in date_rows]
+
+    Conn.close()
+
+    return render_template(
+        "dashboard.html",
+        Total=Total,
+        Moyenne=Moyenne,
+        Categories=Categories,
+        Dates=Dates
+    )
+
+if __name__ == "__main__":
+    App.run(debug=True)
